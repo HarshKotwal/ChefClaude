a7bc809d45f7d3897be8073673a960e67a1cbef1
HarshKotwal <harshkotwal1608@gmail.com>
2025-12-29 14:21:06 +0530

    deployment changes

--- server/index.js
+++ server/index.js
@@ -12,4 +12,5 @@ app.post("/api/recipe", async (req, res) => {
   res.json({ recipe });
 });
 
-app.listen(3001, () => console.log("Server running on port 3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
